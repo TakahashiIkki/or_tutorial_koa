4915d48c989c47f55c18dd28faa7577404526f6b
Takashi Ikki <new_ittyandesu@outlook.jp>
2021-03-22 21:50:17 +0900

    コメントを調整した

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
+// HTTP server を return する メソッド.
+// server.listen() の syntax sugar だったりする
 app.listen(1234, () => {
   console.log('Server is running on port 1234')
 });
